4ba4ef288bede0fb6662e33df86694c440ff0e42
Hristina <malkata_hrisi@abv.bg>
2017-05-12 21:30:20 +0300

    Test Project For Google Maps

---
+++ GoogleMaps/GoogleMaps/GoogleMapsTests.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using GoogleMaps.HomePage;
+using OpenQA.Selenium.Support.UI;
+
+namespace GoogleMapsTest
+{
+    [TestFixture]
+    public class GoogleMapsTests
+    {
+        private IWebDriver driver;
+
+        //Open Chrome browser
+        [SetUp]
+        public void Init()
+        {
+            this.driver = new ChromeDriver();
+            this.driver.Manage().Window.Maximize();
+        }
+
+        //Close Chrome browser
+        [TearDown]
+        public void CleanUp()
+        {
+            this.driver.Quit();
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Hristinka Mineva")]
+
+        //Open Chrome browser and check www.maps.google.com is loaded
+        public void CheckSiteIsLoaded()
+        {
+            var homePage = new HomePage(this.driver);
+            homePage.NavigateTo();
+
+            homePage.AssertSiteIsLoaded("http://maps.google.com");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Hristinka Mineva")]
+
+        //Open www.maps.google.com and check Directions button is visible
+        public void CheckDirectionButtonDisplayed()
+        {
+            var homePage = new HomePage(this.driver);
+            homePage.NavigateTo();
+
+            homePage.AssertDirectionButtonExsists("Directions button is visible!");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Hristinka Mineva")]
+
+        //Click to Directions button and check Direction view is loaded
+        public void CheckDirectionViewIsLoaded()
+        {
+            var homePage = new HomePage(this.driver);
+            homePage.NavigateTo();
+            homePage.DirectionButton.Click();
+
+            homePage.AssertDirectionMenuIsLoaded("Direction menu is loaded!");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Hristinka Mineva")]
+
+        //Click to Transit button and check the time between start and final location
+        public void CheckTheTimeBetweenStartAndFinalLocationWithTransit()
+        {
+            var homePage = new HomePage(this.driver);
+            homePage.NavigateTo();
+            homePage.DirectionButton.Click();
+            homePage.StartLocation.SendKeys("6480 Weathers Pl, San Diego, CA 92121, USA");
+            homePage.FinalLocation.SendKeys("Bus Access Rd, San Diego, CA 92122, USA");
+            homePage.TransitButton.Click();
+
+            homePage.AssertTimeForTravelWithTransit("30 мин");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Hristinka Mineva")]
+
+        //Click to Driving button and check the distance between start and final location
+        public void CheckTheDistanceBetweenStartAndFinalLocationWithDriving()
+        {
+            var homePage = new HomePage(this.driver);
+            homePage.NavigateTo();
+            homePage.DirectionButton.Click();
+            homePage.StartLocation.SendKeys("6480 Weathers Pl, San Diego, CA 92121, USA");
+            homePage.FinalLocation.SendKeys("Bus Access Rd, San Diego, CA 92122, USA");
+            homePage.DrivingButton.Click();
+
+            homePage.AssertDistanceBetweenStartAndFinalLocationWithDriving("4,6 мили");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Hristinka Mineva")]
+
+        //Click to Walking button and check the distance and the time betwee start and final location
+        public void CheckTheDistanceAndTheTimeBetweenStartAndFinalLocationWithWalking()
+        {
+            var homePage = new HomePage(this.driver);
+            homePage.NavigateTo();
+            homePage.DirectionButton.Click();
+            homePage.StartLocation.SendKeys("6480 Weathers Pl, San Diego, CA 92121, USA");
+            homePage.FinalLocation.SendKeys("Bus Access Rd, San Diego, CA 92122, USA");
+            homePage.WalkingButton.Click();
+
+            homePage.AssertDistanceBetweenStartAndFinalLocationWithWalking("5,6 мили");
+            homePage.AssertTimeBetweenStartAndFinalLocationWithWalking("1 ч 53 мин");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Hristinka Mineva")]
+
+        //Click to Cycling button and check check the highest altitude
+        public void CheckTheHighestAltitudeBetweenStartAndFinalLocationWithCycling()
+        {
+            var homePage = new HomePage(this.driver);
+            homePage.NavigateTo();
+            homePage.DirectionButton.Click();
+            homePage.StartLocation.SendKeys("6480 Weathers Pl, San Diego, CA 92121, USA");
+            homePage.FinalLocation.SendKeys("Bus Access Rd, San Diego, CA 92122, USA");
+            homePage.CyclingButton.Click();
+
+            homePage.AssertHighestAltitudeBetweenStartAndFinalLocationWithCycling("417 фута");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Hristinka Mineva")]
+
+        // Send direction to your phone
+        public void CheckSendDirectionToPhoneWithTransit()
+        {
+            var homePage = new HomePage(this.driver);
+            var startLocation = "6480 Weathers Pl, San Diego, CA 92121, USA";
+            var finalLocation = "Bus Access Rd, San Diego, CA 92122, USA";
+            homePage.NavigateTo();
+            LoginGoogleMaps("xpucu91@gmail.com", "76a1512a");
+            homePage.DirectionButton.Click();
+            homePage.StartLocation.SendKeys(startLocation);
+            homePage.FinalLocation.SendKeys(finalLocation);
+            homePage.TransitButton.Click();
+            homePage.SendingButton.Click();
+            homePage.PhoneModel.Click();
+
+            homePage.AssertCheckButtonForSendingMessageDisplayed("The message was send!");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Hristinka Mineva")]
+
+        // Open Schedule Explorer and check the time and the date
+        public void CheckPlanAndScheduleToUser()
+        {
+            var homePage = new HomePage(this.driver);
+            var startLocation = "6480 Weathers Pl, San Diego, CA 92121, USA";
+            var finalLocation = "Bus Access Rd, San Diego, CA 92122, USA";
+            var time = "9:33 AM";
+            homePage.NavigateTo();
+            LoginGoogleMaps("hristina.hrisi.mineva@gmail.com", "qwertyuiop789");
+            homePage.DirectionButton.Click();
+            Type(homePage.StartLocation, startLocation);
+            Type(homePage.FinalLocation, finalLocation);
+            homePage.TransitButton.Click();
+            homePage.OptionButton.Click();
+            homePage.CheckBusButton.Click();
+            homePage.CheckTrainButton.Click();
+            homePage.CheckFewerTransfersButton.Click();
+            homePage.CloseButton.Click();
+            homePage.LeaveNowDropDownButton.Click();
+            homePage.LeaveNowDropDownMenu.Click();
+            Type(homePage.TimeDropDownMenu, time);
+            homePage.ScheduleExplorerButton.Click();
+
+            homePage.AssertTimeScheduleIsCorrect(time);
+        }
+        private void LoginGoogleMaps(string email, string password)
+        {
+            var homePage = new HomePage(this.driver);
+            if (homePage.LoginButton.Displayed)
+            {
+                homePage.LoginButton.Click();
+                homePage.Email.SendKeys(email);
+                homePage.EmailNextButton.Click();
+                homePage.Password.SendKeys(password);
+                homePage.PasswordNextButton.Click();
+            }
+        }
+        private void Type(IWebElement element, string text)
+        {
+            element.Clear();
+            element.SendKeys(text);
+        }
+    }
+}
---
+++ GoogleMaps/GoogleMaps/Pages/HomePage/HomePageMap.cs
@@ -0,0 +1,283 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMaps.HomePage
+{
+    public partial class HomePage : BasePage
+    {
+        public IWebElement LoginButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"gb_70\"]";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement Email
+        {
+            get
+            {
+                var xpath = "//*[@id=\"identifierId\"]";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement EmailNextButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"identifierNext\"]/content/span";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement PasswordNextButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"passwordNext\"]/content/span";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement Password
+        {
+            get
+            {
+                var xpath = "//*[@id=\"password\"]/div[1]/div/div[1]/input";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement DirectionButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"searchbox-directions\"]";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement DirectionMenu
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div"));
+            }
+        }
+
+        public IWebElement TransitButton
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"omnibox-directions\"]/div/div[2]/div/div/div[1]/div[3]/button/div[1]")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"omnibox-directions\"]/div/div[2]/div/div/div[1]/div[3]/button/div[1]"));
+            }
+        }
+
+        public IWebElement StartLocation
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"sb_ifc51\"]/input")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"sb_ifc51\"]/input"));
+            }
+        }
+
+        public IWebElement FinalLocation
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"sb_ifc52\"]/input")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"sb_ifc52\"]/input"));
+            }
+        }
+
+        public IWebElement TimeForTravelWithTransit
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[5]/div/div[2]/div[2]/div[1]/div")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[5]/div/div[2]/div[2]/div[1]/div"));
+            }
+        }
+        public IWebElement DrivingButton
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"omnibox-directions\"]/div/div[2]/div/div/div[1]/div[2]/button/div[1]")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"omnibox-directions\"]/div/div[2]/div/div/div[1]/div[2]/button/div[1]"));
+            }
+        }
+        public IWebElement DistanceBetweenStartAndFinalLocationWithDriving
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[5]/div[1]/div[2]/div[1]/div[1]/div[2]/div")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[5]/div[1]/div[2]/div[1]/div[1]/div[2]/div"));
+            }
+        }
+        public IWebElement WalkingButton
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"omnibox-directions\"]/div/div[2]/div/div/div[1]/div[4]/button/div[1]")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"omnibox-directions\"]/div/div[2]/div/div/div[1]/div[4]/button/div[1]"));
+            }
+        }
+        public IWebElement DistanceBetweenStartAndFinalLocationWithWalking
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[5]/div[1]/div[2]/div[3]/div[1]/div[2]")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[5]/div[1]/div[2]/div[3]/div[1]/div[2]"));
+            }
+        }
+        public IWebElement TimeBetweenStartAndFinalLocationWithWalking
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[5]/div[1]/div[2]/div[3]/div[1]/div[1]")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[5]/div[1]/div[2]/div[3]/div[1]/div[1]"));
+            }
+        }
+        public IWebElement CyclingButton
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"omnibox-directions\"]/div/div[2]/div/div/div[2]/div[1]/div[1]/button/div[1]")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"omnibox-directions\"]/div/div[2]/div/div/div[2]/div[1]/div[1]/button/div[1]"));
+            }
+        }
+        public IWebElement HighestAltitudeBetweenStartAndFinalLocationWithCycling
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[5]/div[1]/div[2]/div[3]/div[3]/div/div/span[1]")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[5]/div[1]/div[2]/div[3]/div[3]/div/div/span[1]"));
+            }
+        }
+        public IWebElement SendingButton
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[4]/div[2]/div[2]/button")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[4]/div[2]/div[2]/button"));
+            }
+        }
+        public IWebElement PhoneModel
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"modal-dialog-widget\"]/div[2]/div/div[3]/div/div/div[1]/div[1]/div/span[3]/span[1]/span[1]/button")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"modal-dialog-widget\"]/div[2]/div/div[3]/div/div/div[1]/div[1]/div/span[3]/span[1]/span[1]/button"));
+            }
+        }
+        public IWebElement CheckButton
+        {
+            get
+            {
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[4]/div[2]/div[1]")));
+                return this.Driver.FindElement(By.XPath("//*[@id=\"pane\"]/div/div[2]/div/div/div[4]/div[2]/div[1]"));
+            }
+        }
+        public IWebElement LeaveNowDropDownButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"pane\"]/div/div[2]/div/div/div[2]/span/div/div/div/div[2]";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement LeaveNowDropDownMenu
+        {
+            get
+            {
+                var xpath = "//*[@id=\":1\"]/div";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement TimeDropDownMenu
+        {
+            get
+            {
+                var xpath = "//input[@name=\"transit-time\"]"; 
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement OptionButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"pane\"]/div/div[2]/div/div/div[2]/button[3]/span[1]";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement CheckBusButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"pane\"]/div/div[2]/div/div/div[2]/div[2]/div/div[3]/div[1]/div[1]/label";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement CheckTrainButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"pane\"]/div/div[2]/div/div/div[2]/div[2]/div/div[3]/div[1]/div[3]/label";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement CheckFewerTransfersButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"pane\"]/div/div[2]/div/div/div[2]/div[2]/div/div[3]/div[2]/label[2]";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement CloseButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"pane\"]/div/div[2]/div/div/div[2]/button[3]/span[2]";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement ScheduleExplorerButton
+        {
+            get
+            {
+                var xpath = "//*[@id=\"pane\"]/div/div[2]/div/div/div[6]/button";
+                this.Wait.Until(ExpectedConditions.ElementExists(By.XPath(xpath)));
+                return this.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+        public IWebElement TimeScheduleExplorer
+        {
+            get
+            {
+                var className = "time-input"; 
+                this.Wait.Until(ExpectedConditions.ElementExists(By.ClassName(className)));
+                return this.Driver.FindElement(By.ClassName(className));
+            }
+        }
+    }
+}
